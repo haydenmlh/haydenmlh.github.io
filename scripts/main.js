9cef93ac4e944ca8f1d63da91ec11b5af3a7675d
Hayden M <haydenmlh@gmail.com>
2021-06-08 04:11:18 -0400

    Mostly finished the header

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("buttons");
-    if (x.className === "buttons") {
+    if (x.className === "") {
       x.className += " responsive";
     } else {
-      x.className = "buttons";
+      x.className = "";
     }
-  }
+}
