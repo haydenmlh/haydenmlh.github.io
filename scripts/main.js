bf857c4c11f0da4aeba2df6dad2f8808eabe1d59
Hayden M <haydenmlh@gmail.com>
2021-06-10 05:03:48 -0400

    Updated the home page slideshow

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,20 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("buttons");
-    if (x.className === "noselect") {
-      x.className += " responsive";
+    if (x.className === "") {
+      x.className += "responsive";
     } else {
-      x.className = "noselect";
+      x.className = "";
     }
+}
+
+document.getElementById("body").onscroll = function myFunction() {  
+    var scrolltotop = document.scrollingElement.scrollTop;
+    var target = document.getElementById("slideshow");
+    var width = target.offsetWidth;
+    var xvalue = - 0.04 * scrolltotop;
+    var factor = 0.2;
+    var yvalue = scrolltotop * factor;
+    target.style.backgroundPosition = xvalue + "px " + yvalue + "px";
 }
 
