a5ac61a2a0b6dbf74dad2d0352f57099e67eb854
Hayden M <haydenmlh@gmail.com>
2021-06-10 13:44:59 -0400

    Update slideshow

--- scripts/image_scroll.js
+++ scripts/image_scroll.js
@@ -1,7 +1,8 @@
 document.getElementById("body").onscroll = function myFunction() {  
     var scrolltotop = document.scrollingElement.scrollTop;
     var targets = document.getElementsByClassName("image-scroll");
-    var xvalue = -0.04 * scrolltotop;
+    var middle = targets[0].offsetHeight / 2;
+    var xvalue = -0.04 * scrolltotop - middle;
     var factor = 0.2;
     var yvalue = scrolltotop * factor;
     for (var i = 0; i < targets.length; i++) {
