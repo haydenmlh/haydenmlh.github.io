10ba8f012534a237a901637230f5f5efaa5752ce
Hayden M <haydenmlh@gmail.com>
2021-06-11 13:58:45 -0400

    Change preload to the correct images

--- scripts/image_scroll.js
+++ scripts/image_scroll.js
@@ -15,9 +15,9 @@ function resizeSlideshow() {
 }
 
 function preLoader() {
-    document.getElementById("preload-01").style.background = 'url("/images/me_1.jpg") no-repeat -9999px -9999px';
-    document.getElementById("preload-02").style.background = 'url("/images/me_2.jpg") no-repeat -9999px -9999px';
-    document.getElementById("preload-03").style.background = 'url("/images/me_3.jpg") no-repeat -9999px -9999px';
+    document.getElementById("preload-01").style.background = 'url("/images/me_2.jpg") no-repeat -9999px -9999px';
+    document.getElementById("preload-02").style.background = 'url("/images/me_3.jpg") no-repeat -9999px -9999px';
+    document.getElementById("preload-03").style.background = 'url("/images/me_4.jpg") no-repeat -9999px -9999px';
 }
 
 resizeSlideshow();
