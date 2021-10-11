709aa5b858a606f94a4bd63b8aa9a253882d0c58
Hayden M <haydenmlh@gmail.com>
2021-10-11 01:43:35 -0400

    Update projects page
    Add 207 project to the projects page and add an image to accompany it.

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,12 +1,14 @@
 function changeWrapperHW() {
     var tiles=document.getElementsByClassName('tile');
     var tile_titles = document.getElementsByClassName('tile-title');
-    var tile_desc = document.getElementsByClassName('tile-description');
+    var tile_langs = document.getElementsByClassName('tile-lang')
+    var tile_descs = document.getElementsByClassName('tile-description');
     
     for (i = 0; i < tiles.length; i++) {
         title_height = tile_titles[i].offsetHeight;
-        desc_height = tile_desc[i].offsetHeight;
-        var target = tiles[i].offsetWidth + title_height + desc_height - 20;
+        desc_height = tile_descs[i].offsetHeight;
+        lang_height = tile_langs[i].offsetHeight;
+        var target = tiles[i].offsetWidth + title_height + desc_height + lang_height - 20;
         tiles[i].style.height = target + "px";
     }    
     
