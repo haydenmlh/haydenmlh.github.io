67edaf865bb07d62771acdfb58cb610688624014
Hayden Mak <haydenmlh@gmail.com>
2024-02-21 02:02:17 -0500

    Update Layout

--- scripts/preloader_main.js
+++
@@ -1,6 +0,0 @@
-function preLoader() {
-    document.getElementById("preload-01").style.background = 'url("/images/me_2.jpg") no-repeat -9999px -9999px';
-    document.getElementById("preload-02").style.background = 'url("/images/me_3.jpg") no-repeat -9999px -9999px';
-    document.getElementById("preload-03").style.background = 'url("/images/me_4.jpg") no-repeat -9999px -9999px';
-}
-preLoader();
